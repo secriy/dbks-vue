6c834aa43fa298b73546723b0f89e75606468c37
Secriy <secriyal@gmail.com>
2020-07-04 14:27:43 +0800

    修复错误

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,11 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 Vue.use(Router);
+// 解决重复点击导航路由报错
+const originalPush = Router.prototype.push;
+Router.prototype.push = function push(location) {
+    return originalPush.call(this, location).catch(err => err);
+};
 
 export default new Router({
     routes: [
