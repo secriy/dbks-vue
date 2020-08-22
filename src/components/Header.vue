0480761381a9e85cb731e64f59ba1d744d40b39e
Secriy <secriyal@gmail.com>
2020-08-22 15:20:47 +0800

    图片资源修改

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,7 +16,7 @@
                 </div>
                 <!-- 用户头像 -->
                 <div class="user-avator">
-                    <img src="../assets/img/avator.png" />
+                    <img src="../assets/images/avator.png" />
                 </div>
                 <!-- 用户名下拉菜单 -->
                 <el-dropdown class="user-name" trigger="click" @command="handleCommand">
